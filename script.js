fae08d1e57f3d0ded4e02a36a96609cf7e795a3f
joshua-gibson <joshua.gibson.dev@gmail.com>
2023-05-19 21:21:11 +0100

    output given a valid date looks correct

--- script.js
+++ script.js
@@ -3,26 +3,60 @@ const day = document.getElementById("day").value;
 const month = document.getElementById("month").value;
 const year = document.getElementById("year").value;
 
-function subtractYears(date, years) {
-  date.setFullYear(date.getFullYear() - years);
-
-  return date;
+function monthDiff(d1, d2) {
+  var months;
+  months = (d2.getFullYear() - d1.getFullYear()) * 12;
+  months -= d1.getMonth();
+  months += d2.getMonth();
+  return months <= 0 ? 0 : months;
 }
 
-function myFunction(day, month, year) {
-  const inputDate = new Date(day + "/" + month + "/" + year);
-  const currentDate = new Date();
-  // how many years between dates
-  const age = currentDate.getFullYear() - inputDate.getFullYear();
-  //subtract age from current date
-  const newDiff = new Date(currentDate - inputDate);
+function daysDiff(date1, date2) {
+  const ONE_DAY = 1000 * 60 * 60 * 24;
+  const differenceMs = Math.abs(date1 - date2);
+  return Math.round(differenceMs / ONE_DAY);
+}
 
-  console.log(age);
-  return age;
+function getDateDifferences(day, month, year) {
+  const inputDate = new Date(day + "/" + month + "/" + year);
+
+  const currentDate = new Date();
+  const currDay = currentDate.getDate();
+  const currMonth = currentDate.getMonth();
+  const currYear = currentDate.getYear() + 1900;
+  // how many years between dates
+  const yearsSince = currentDate.getFullYear() - inputDate.getFullYear();
+  //subtract years from current date
+  const removeYearssDate = new Date(
+    currYear - yearsSince,
+    currMonth,
+    currDay,
+    0,
+    0,
+    0,
+    0
+  );
+  // how many months between dates
+  const monthsSince = monthDiff(inputDate, removeYearssDate) + 1;
+  //subtract months from current date
+  const removeMonthsDate = new Date(
+    currYear - yearsSince,
+    currMonth + 1 - monthsSince,
+    currDay,
+    0,
+    0,
+    0,
+    0
+  );
+  // how many days between dates
+  const daysSince = daysDiff(inputDate, removeMonthsDate);
+
+  return { yearsSince, monthsSince, daysSince };
 }
 
 fetchBtn.addEventListener("click", () => {
-  myFunction(day, month, year);
+  const dateDifferences = getDateDifferences(day, month, year);
+  document.getElementById("years").innerHTML = dateDifferences.yearsSince;
+  document.getElementById("months").innerHTML = dateDifferences.monthsSince;
+  document.getElementById("days").innerHTML = dateDifferences.daysSince;
 });
-
-export { subtractYears };
