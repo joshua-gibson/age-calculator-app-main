313dd4be16619748398ad6bce87b264c9d0ba421
joshua-gibson <joshua.gibson.dev@gmail.com>
2023-05-20 15:27:45 +0100

    error messages seem to be working

--- script.js
+++ script.js
@@ -14,7 +14,60 @@ function daysDiff(date1, date2) {
   return Math.round(differenceMs / ONE_DAY);
 }
 
+function errorMsg(day, month, year) {
+  // create a date object for the current date
+  const currentDate = new Date();
+  const currDay = currentDate.getDate();
+  const currMonth = currentDate.getMonth();
+  const currYear = currentDate.getYear() + 1900;
+  var yearMsg = "";
+  var monthMsg = "";
+  var dayMsg = "";
+  // error msg if the year is not valid or in the future
+  if (
+    isNaN(parseInt(year)) ||
+    parseInt(year) <= 0 ||
+    parseInt(year) > currYear
+  ) {
+    yearMsg = "Please enter a valid year";
+  }
+  if (parseInt(year) > currYear) {
+    yearMsg = "Year must be in the past";
+  }
+  // error msg if the month is not valid or in the future
+  if (isNaN(parseInt(month)) || parseInt(month) + 1 <= 0) {
+    monthMsg = "Please enter a valid month";
+  }
+  if (parseInt(year) === currYear && parseInt(month) > currMonth) {
+    monthMsg = "Month must be in the past";
+  }
+  // combine the day, month and year into a date
+  const providedDate = new Date(year, month, day, 0, 0, 0, 0);
+  //check the date is valid and not in the future
+  if (
+    isNaN(parseInt(day)) ||
+    parseInt(day) < 1 ||
+    parseInt(day) > 31 ||
+    (monthMsg.length === 0 &&
+      yearMsg.length === 0 &&
+      (!providedDate instanceof Date || isNaN(providedDate)))
+  ) {
+    dayMsg = "Please enter a valid day";
+  }
+  if (
+    parseInt(year) === currYear &&
+    parseInt(month) === currMonth &&
+    parseInt(day) > currDay
+  ) {
+    dayMsg = "Day must be in the past";
+  }
+
+  return { yearMsg, monthMsg, dayMsg };
+}
+
 function getDateDifferences(day, month, year) {
+  const errors = errorMsg(day, month, year);
+
   const inputDate = new Date(day + "/" + month + "/" + year);
 
   const currentDate = new Date();
@@ -48,7 +101,12 @@ function getDateDifferences(day, month, year) {
   // how many days between dates
   const daysSince = daysDiff(inputDate, removeMonthsDate);
 
+  // error trapping
+
   return {
+    yearError: errors.yearMsg,
+    monthError: errors.monthMsg,
+    dayError: errors.dayMsg,
     yearsSince,
     yearsSinceText: yearsSince === 1 ? "year" : "years",
     monthsSince,
@@ -59,20 +117,22 @@ function getDateDifferences(day, month, year) {
 }
 
 fetchBtn.addEventListener("click", () => {
-  console.log("clicked");
   const day = document.getElementById("day").value;
   const month = document.getElementById("month").value;
   const year = document.getElementById("year").value;
   const dateDifferences = getDateDifferences(day, month, year);
-  console.log(dateDifferences);
+
+  document.getElementById("years-error").innerHTML = dateDifferences.yearError;
+  document.getElementById("months-error").innerHTML =
+    dateDifferences.monthError;
+  document.getElementById("days-error").innerHTML = dateDifferences.dayError;
+
   document.getElementById("years").innerHTML = dateDifferences.yearsSince;
   document.getElementById("years-text").innerHTML =
     dateDifferences.yearsSinceText;
-
   document.getElementById("months").innerHTML = dateDifferences.monthsSince;
   document.getElementById("months-text").innerHTML =
     dateDifferences.monthsSinceText;
-
   document.getElementById("days").innerHTML = dateDifferences.daysSince;
   document.getElementById("days-text").innerHTML =
     dateDifferences.daysSinceText;
